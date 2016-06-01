baaa1b572bb5ae6d0624c03b3f72b9cbc40de686
Markus Armbruster <armbru@pond.sub.org>
2016-06-01 21:29:55 +0200

    update: Avoid unnecessary inexactness in total_work()
    Commit 5ba8cab (v4.2.20) replaced milit * 2 / 5.0 by milit * 0.4.  Not
    so smart; 0.4 isn't exact.  Go back to milit / 2.5.
    
    Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

--- src/lib/update/populace.c
+++ src/lib/update/populace.c
@@ -124,5 +124,5 @@ total_work(int sctwork, int etu, int civil, int milit, int uw, int maxpop)
     if (uw > maxpop)
 	uw = maxpop;
 
-    return (civil * sctwork / 100.0 + milit * 0.4 + uw) * etu / 100.0;
+    return (civil * sctwork / 100.0 + milit / 2.5 + uw) * etu / 100.0;
 }
