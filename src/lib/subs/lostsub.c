1baca48a337eb6a501375ebbaa3f3a810ffed97f
Markus Armbruster <armbru@pond.sub.org>
2016-06-04 17:38:25 +0200

    subs: Oops on makelost() / makenotlost() with no owner
    This catches bogus entries in the "lost" file like the ones fixed in
    the previous commit.
    
    Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

--- src/lib/subs/lostsub.c
+++ src/lib/subs/lostsub.c
@@ -60,6 +60,8 @@ makelost(int type, natid owner, int id, coord x, coord y)
     struct loststr lost;
     int n;
 
+    if (CANT_HAPPEN(!owner))
+	return;
     n = findlost(type, owner, id, x, y, 1);
     ef_blank(EF_LOST, n, &lost);
     lost.lost_type = type;
@@ -76,6 +78,8 @@ makenotlost(int type, natid owner, int id, coord x, coord y)
     struct loststr lost;
     int n;
 
+    if (CANT_HAPPEN(!owner))
+	return;
     n = findlost(type, owner, id, x, y, 0);
     if (n < 0)
 	return;
