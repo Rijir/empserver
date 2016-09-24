Rijir/empserver

commit 44c36fa7d5ad382e11483e6d82d896efdf22016e

Make sector maintenance cost configurable
Replace the fixed $1 per ETU maintenance for capital/city sectors that
are at least 60% efficient by a configurable maintenance cost, payable
regardless of efficiency.  The only change in the default
configuration is that inefficient capitals now pay maintenance.
Charging sector maintenance regardless of efficiency is consistent
with unit maintenance.

New struct dchrstr member d_maint and sector-chr selector maint.  Make
show_sect_build() show it.  Change produce_sect() to record
maintenance in new slot p_sect[SCT_MAINT] instead of abusing
p_sect[SCT_CAPIT].  Replace the "Capital maintenance" line in budget
by "Sector maintenance".

src/lib/update/sect.c
@@ -278,13 +278,6 @@ produce_sect(int natnum, int etu, struct bp *bp, long p_sect[][2])
 
 	np = getnatp(natnum);
 
-	if (sp->sct_type == SCT_CAPIT && sp->sct_effic >= 60) {
-	    p_sect[SCT_CAPIT][0]++;
-	    p_sect[SCT_CAPIT][1] += etu;
-	    if (!player->simulation)
-		np->nat_money -= etu;
-	}
-
 	if (player->simulation) {
 	    /* work on a copy, which will be discarded */
 	    memcpy(buf, sp->sct_item, sizeof(buf));
@@ -320,6 +313,14 @@ produce_sect(int natnum, int etu, struct bp *bp, long p_sect[][2])
 	pcost = cost = ecost = 0;
 
 	desig = sp->sct_type;
+
+	if (dchr[desig].d_maint) {
+	    cost = etu * dchr[desig].d_maint;
+	    p_sect[SCT_MAINT][0]++;
+	    p_sect[SCT_MAINT][1] += cost;
+	    if (!player->simulation)
+		np->nat_money -= cost;
+	}
 
 	if ((sp->sct_effic < 100 || sp->sct_type != sp->sct_newtype) &&
 	    np->nat_money >= 0) {

include/budg.h
@@ -38,7 +38,8 @@
 #include "item.h"
 
 #define SCT_EFFIC (SCT_TYPE_MAX + 1)
-#define SCT_BUDG_MAX SCT_EFFIC
+#define SCT_MAINT (SCT_TYPE_MAX + 2)
+#define SCT_BUDG_MAX SCT_MAINT
 
 struct bp *bp_alloc(void);
 void bp_set_from_sect(struct bp *, struct sctstr *);

include/sect.h
@@ -116,6 +116,7 @@ struct dchrstr {
     int d_build;		/* cost multiplier for eff */
     int d_lcms;			/* lcm's needed per point of eff */
     int d_hcms;			/* hcm's needed per point of eff */
+    int d_maint;		/* maintenance cost per ETU */
     int d_maxpop;		/* maximum population */
     char *d_name;		/* full name of sector type */
 };

info/Sector-types.t
@@ -237,11 +237,12 @@ The \*Qshow\*U command can be used to show special things about sector
 types.  To check out build costs, you do:
 .EX show sect build
 .NF
-                        desig   build 100% eff
-sector type                 $   lcm   hcm     $
-- wilderness                0     0     0     0
-f fortress                  0     0   100   500
-any other                   0     0     0   100
+                        desig   build 100% eff  maint
+sector type                 $   lcm  hcm     $      $
+- wilderness                0     0    0     0      0
+c capital                   0     0    0   100     60
+f fortress                  0     0  100   500      0
+any other                   0     0    0   100      0
 
 Infrastructure building - adding 1 point of efficiency costs:
        type          lcms    hcms    mobility    $$$$
@@ -259,6 +260,8 @@ The lcm required to build the sector.
 The hcm required to build the sector.
 .L "build $"
 What it costs to build the sector.
+.L "maint $"
+Cost of maintenance per update.
 .in
 .s1
 The show command also shows infrastructure build costs.

info/Update-sequence.t
@@ -24,17 +24,16 @@ This document gives a rough order of events during the update.
 	d) land unit maintenance
 		pay maintenance, then feed and plague people on board
 	e) produce for all sectors
-                a) if the sector is a cap, it costs $1 per etu since
-			the last update
-                b) people in non-sanctuary sectors eat
+                a) people in non-sanctuary sectors eat
                         If not enough is available, the excess people will
 			starve off.  No more than 50% of the people
                         in a sector can die; the uw's die first, then
                         the civs, then the mil.
-                c) if there was starvation, the work percentage is set
+                b) if there was starvation, the work percentage is set
                         to 0, otherwise the work rate rises by
                         8+(1-15), max of 100
-                d) population grows and is truncated
+                c) population grows and is truncated
+                d) pay sector maintenance
                 e) sectors that are stopped are skipped (see info stop)
                 f) first increase eff
                 g) then make things

commit 9f1c2d7ff726a674e940da19faa2f53dbc989f1c

update: Fix sector maintenance when stopped or broke
No maintenance is paid when the sector is stopped or the owner is
broke.  Broken in commit 44c36fa, v4.3.23.  Fix it.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

src/lib/update/sect.c
@@ -257,17 +257,17 @@ produce_sect(struct natstr *np, int etu, struct bp *bp)
 
 	do_feed(sp, np, etu, 0);
 
-	if (sp->sct_off || budget->money < 0) {
-	    sp->sct_avail = 0;
-	    bp_set_from_sect(bp, sp);
-	    continue;
-	}
-
 	if (dchr[sp->sct_type].d_maint) {
 	    cost = etu * dchr[sp->sct_type].d_maint;
 	    budget->bm[BUDG_SCT_MAINT].count++;
 	    budget->bm[BUDG_SCT_MAINT].money -= cost;
 	    budget->money -= cost;
+	}
+
+	if (sp->sct_off || budget->money < 0) {
+	    sp->sct_avail = 0;
+	    bp_set_from_sect(bp, sp);
+	    continue;
 	}
 
 	if ((sp->sct_effic < 100 || sp->sct_type != sp->sct_newtype) &&

tests/update/04-4
@@ -1,5 +1,4 @@
 budget
-| BUG: stopped city pays no maintenance
 | TODO is it accurate?
 production *
 | TODO is it accurate?

tests/update/99-POGO
@@ -151,7 +151,6 @@ land 0:31,0:15
 | starvation 0:15,0
 | #30 lost 12% to lack of maintenance
 | #31 lost 1%
-| BUG: no sector maintenance
 | sector building
 | -16:-12,0 no money
 | sector production

tests/update/final.xdump
@@ -412,9 +412,9 @@ config nat
 cnum stat flags cname passwd ip hostname userid xcap ycap xorg yorg update tgms ann timeused btu access milreserve money login logout newstim annotim tech research education happiness relations(0) relations(1) relations(2) relations(3) relations(4) relations(5) relations(6) relations(7) relations(8) relations(9) relations(10) relations(11) relations(12) relations(13) relations(14) relations(15) relations(16) relations(17) relations(18) relations(19) relations(20) relations(21) relations(22) relations(23) relations(24) relations(25) relations(26) relations(27) relations(28) relations(29) relations(30) relations(31) relations(32) relations(33) relations(34) relations(35) relations(36) relations(37) relations(38) relations(39) relations(40) relations(41) relations(42) relations(43) relations(44) relations(45) relations(46) relations(47) relations(48) relations(49) relations(50) relations(51) relations(52) relations(53) relations(54) relations(55) relations(56) relations(57) relations(58) relations(59) relations(60) relations(61) relations(62) relations(63) relations(64) relations(65) relations(66) relations(67) relations(68) relations(69) relations(70) relations(71) relations(72) relations(73) relations(74) relations(75) relations(76) relations(77) relations(78) relations(79) relations(80) relations(81) relations(82) relations(83) relations(84) relations(85) relations(86) relations(87) relations(88) relations(89) relations(90) relations(91) relations(92) relations(93) relations(94) relations(95) relations(96) relations(97) relations(98) contacts(0) contacts(1) contacts(2) contacts(3) contacts(4) contacts(5) contacts(6) contacts(7) contacts(8) contacts(9) contacts(10) contacts(11) contacts(12) contacts(13) contacts(14) contacts(15) contacts(16) contacts(17) contacts(18) contacts(19) contacts(20) contacts(21) contacts(22) contacts(23) contacts(24) contacts(25) contacts(26) contacts(27) contacts(28) contacts(29) contacts(30) contacts(31) contacts(32) contacts(33) contacts(34) contacts(35) contacts(36) contacts(37) contacts(38) contacts(39) contacts(40) contacts(41) contacts(42) contacts(43) contacts(44) contacts(45) contacts(46) contacts(47) contacts(48) contacts(49) contacts(50) contacts(51) contacts(52) contacts(53) contacts(54) contacts(55) contacts(56) contacts(57) contacts(58) contacts(59) contacts(60) contacts(61) contacts(62) contacts(63) contacts(64) contacts(65) contacts(66) contacts(67) contacts(68) contacts(69) contacts(70) contacts(71) contacts(72) contacts(73) contacts(74) contacts(75) contacts(76) contacts(77) contacts(78) contacts(79) contacts(80) contacts(81) contacts(82) contacts(83) contacts(84) contacts(85) contacts(86) contacts(87) contacts(88) contacts(89) contacts(90) contacts(91) contacts(92) contacts(93) contacts(94) contacts(95) contacts(96) contacts(97) contacts(98) rejects(0) rejects(1) rejects(2) rejects(3) rejects(4) rejects(5) rejects(6) rejects(7) rejects(8) rejects(9) rejects(10) rejects(11) rejects(12) rejects(13) rejects(14) rejects(15) rejects(16) rejects(17) rejects(18) rejects(19) rejects(20) rejects(21) rejects(22) rejects(23) rejects(24) rejects(25) rejects(26) rejects(27) rejects(28) rejects(29) rejects(30) rejects(31) rejects(32) rejects(33) rejects(34) rejects(35) rejects(36) rejects(37) rejects(38) rejects(39) rejects(40) rejects(41) rejects(42) rejects(43) rejects(44) rejects(45) rejects(46) rejects(47) rejects(48) rejects(49) rejects(50) rejects(51) rejects(52) rejects(53) rejects(54) rejects(55) rejects(56) rejects(57) rejects(58) rejects(59) rejects(60) rejects(61) rejects(62) rejects(63) rejects(64) rejects(65) rejects(66) rejects(67) rejects(68) rejects(69) rejects(70) rejects(71) rejects(72) rejects(73) rejects(74) rejects(75) rejects(76) rejects(77) rejects(78) rejects(79) rejects(80) rejects(81) rejects(82) rejects(83) rejects(84) rejects(85) rejects(86) rejects(87) rejects(88) rejects(89) rejects(90) rejects(91) rejects(92) rejects(93) rejects(94) rejects(95) rejects(96) rejects(97) rejects(98)
 0 deity (flash beep coastwatch sonar techlists) "POGO" "peter" "127.0.0.1" "" "tester" 0 0 0 0 0 0 0 255 640 0 0 123456789 0 0 0 0 0.00000 0.00000 0.00000 0.00000 neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () ()
 1 active (flash beep coastwatch sonar techlists) "1" "1" "127.0.0.1" "" "tester" 0 0 0 0 0 1 0 255 640 0 0 12733 0 0 0 0 102.920 3.48828 21.0871 12.3282 neutral neutral neutral neutral neutral neutral allied friendly neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () ()
-2 active (flash beep coastwatch sonar techlists) "2" "2" "127.0.0.1" "" "tester" -2 0 0 0 0 1 0 255 193 0 975 -5921 0 0 0 0 101.147 1.74414 15.2381 2.74222 neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () ()
+2 active (flash beep coastwatch sonar techlists) "2" "2" "127.0.0.1" "" "tester" -2 0 0 0 0 1 0 255 193 0 975 -6221 0 0 0 0 101.147 1.74414 15.2381 2.74222 neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () ()
 3 active (flash beep coastwatch sonar techlists) "3" "3" "127.0.0.1" "" "tester" 1 -1 0 0 0 1 0 255 640 0 0 4337 0 0 0 0 101.147 1.74414 15.2381 4.44444 neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () ()
-4 active (flash beep coastwatch sonar techlists) "4" "4" "127.0.0.1" "" "tester" -1 -1 0 0 0 1 0 255 640 0 0 18750 0 0 0 0 31.5848 1.74414 3.04762 4.44444 neutral allied neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () ()
+4 active (flash beep coastwatch sonar techlists) "4" "4" "127.0.0.1" "" "tester" -1 -1 0 0 0 1 0 255 640 0 0 18690 0 0 0 0 31.5848 1.74414 3.04762 4.44444 neutral allied neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () ()
 5 active (flash beep coastwatch sonar techlists) "5" "5" "127.0.0.1" "" "tester" -16 -8 0 0 0 1 0 255 99 0 0 26498 0 0 0 0 101.147 1.74414 15.2381 4.44444 neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () ()
 6 active (flash beep coastwatch sonar techlists) "6" "6" "127.0.0.1" "" "tester" 0 8 0 0 0 1 0 255 640 0 0 24803 0 0 0 0 101.147 1.74414 15.2381 4.44444 allied allied allied allied allied allied neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () ()
 7 active (flash beep coastwatch sonar techlists) "7" "7" "127.0.0.1" "" "tester" -2 8 0 0 0 1 0 255 640 0 0 25819 0 0 0 0 101.147 1.74414 15.2381 4.44444 friendly friendly friendly friendly friendly friendly friendly neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral neutral 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () () ()

tests/update/journal.log
@@ -228,14 +228,15 @@
     Play#2 output Play#2 1 Ship maintenance    		3 ships         		      90
     Play#2 output Play#2 1 Plane maintenance   		3 planes        		     147
     Play#2 output Play#2 1 Unit maintenance    		3 units         		      90
+    Play#2 output Play#2 1 Sector maintenance  		5 sectors       		     300
     Play#2 output Play#2 1 Military payroll		1900 mil, 1000 res                 10000
-    Play#2 output Play#2 1 Total expenses.....................................................10325
+    Play#2 output Play#2 1 Total expenses.....................................................10625
     Play#2 output Play#2 1 Income from taxes		6800 civs, 4000 uws                +1076
     Play#2 output Play#2 1 Income from bars		400 bars                           +2865
     Play#2 output Play#2 1 Total income.......................................................+3941
     Play#2 output Play#2 1 Balance forward						             100
-    Play#2 output Play#2 1 Estimated delta						           -6384
-    Play#2 output Play#2 1 Estimated new treasury.............................................-6284
+    Play#2 output Play#2 1 Estimated delta						           -6684
+    Play#2 output Play#2 1 Estimated new treasury.............................................-6584
     Play#2 output Play#2 1 After processsing sectors, you will be broke!
     Play#2 output Play#2 1 Sectors will not produce, distribute, or deliver!
     Play#2 output Play#2 1
@@ -317,14 +318,15 @@
     Play#4 output Play#4 1 bank             		4 bars   			      40
     Play#4 output Play#4 1 Ship maintenance    		10 ships        		     300
     Play#4 output Play#4 1 Unit maintenance    		10 units        		     600
+    Play#4 output Play#4 1 Sector maintenance  		1 sector        		      60
     Play#4 output Play#4 1 Military payroll		1111 mil, 0 res                     5555
-    Play#4 output Play#4 1 Total expenses......................................................6494
+    Play#4 output Play#4 1 Total expenses......................................................6554
     Play#4 output Play#4 1 Income from taxes		23604 civs, 4100 uws                +212
     Play#4 output Play#4 1 Income from bars		100 bars                           +1500
     Play#4 output Play#4 1 Total income.......................................................+1712
     Play#4 output Play#4 1 Balance forward						           25000
-    Play#4 output Play#4 1 Estimated delta						           -4782
-    Play#4 output Play#4 1 Estimated new treasury.............................................20218
+    Play#4 output Play#4 1 Estimated delta						           -4842
+    Play#4 output Play#4 1 Estimated new treasury.............................................20158
     Play#4 output Play#4 6 0 99
     Play#4 input production *
     Play#4 command production
@@ -896,7 +898,7 @@
     Play#0 output Play#0 1 total pop was 6100, yielding 0.00 hap, 0.00 edu
     Play#0 output Play#0 1 1.7834 technology (0.0000 + 1.7834), 1.7551 research (0.0000 + 1.7551) produced
     Play#0 output Play#0 1 Army delta $0, Navy delta $0, Air force delta $-74
-    Play#0 output Play#0 1 money delta was $-6021 for this update
+    Play#0 output Play#0 1 money delta was $-6321 for this update
     Play#0 output Play#0 6 0 640
     Play#0 input nation 2
     Play#0 command nation
@@ -904,7 +906,7 @@
     Play#0 output Play#0 1 (#2) 2 Nation Report	Thu Jan  1 00:00:00 1970
     Play#0 output Play#0 1 Nation status is ACTIVE     Bureaucratic Time Units: 193
     Play#0 output Play#0 1 10% eff capital at -2,0 has 130 civilians & 0 military
-    Play#0 output Play#0 1  The treasury has $-5921.00     Military reserves: 975
+    Play#0 output Play#0 1  The treasury has $-6221.00     Military reserves: 975
     Play#0 output Play#0 1 Education.......... 15.24       Happiness.......  2.74
     Play#0 output Play#0 1 Technology.........101.15       Research........  1.74
     Play#0 output Play#0 1 Technology factor : 50.19%     Plague factor :   1.98%
@@ -1389,7 +1391,7 @@
     Play#0 output Play#0 1 total pop was 24080, yielding 0.00 hap, 0.00 edu
     Play#0 output Play#0 1 1.7834 technology (0.0000 + 1.7834), 1.7551 research (0.0000 + 1.7551) produced
     Play#0 output Play#0 1 Army delta $-600, Navy delta $-300, Air force delta $0
-    Play#0 output Play#0 1 money delta was $-6250 for this update
+    Play#0 output Play#0 1 money delta was $-6310 for this update
     Play#0 output Play#0 6 0 640
     Play#0 input nation 4
     Play#0 command nation
@@ -1397,7 +1399,7 @@
     Play#0 output Play#0 1 (#4) 4 Nation Report	Thu Jan  1 00:00:00 1970
     Play#0 output Play#0 1 Nation status is ACTIVE     Bureaucratic Time Units: 640
     Play#0 output Play#0 1 100% eff capital at -1,-1 has 130 civilians & 0 military
-    Play#0 output Play#0 1  The treasury has $18750.00     Military reserves: 0
+    Play#0 output Play#0 1  The treasury has $18690.00     Military reserves: 0
     Play#0 output Play#0 1 Education..........  3.05       Happiness.......  4.44
     Play#0 output Play#0 1 Technology......... 31.58       Research........  1.74
     Play#0 output Play#0 1 Technology factor : 35.23%     Plague factor :   1.29%