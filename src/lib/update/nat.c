Rijir/empserver

commit ac25058b50a829c39612d87f05c208718496bea3

(prod_nat): Remove non-functional aging code for nat_contact. Remove nat_contact debugging from server log.
(update_main, prod_nat): Move aging code for nat_contact
from update_main to prod_nat.

src/lib/update/nat.c
@@ -134,7 +134,8 @@ prod_nat(int etu)
     double res[MAXNOC];
     double newvalue;
     natid n;
-    int cn, cont;
+    int cn;
+    struct natstr *cnp;
 
     for (n = 0; NULL != (np = getnatp(n)); n++) {
 	if ((np->nat_stat & STAT_NORM) == 0)
@@ -215,13 +216,9 @@ prod_nat(int etu)
 	wu((natid)0, n, "money delta was $%ld for this update\n",
 	   np->nat_money - money[n]);
 	if (opt_LOSE_CONTACT) {
-	    for (cn = 0; cn <= MAXNOC; cn++) {
-		cont = getcontact(np, cn);
-		if (cont > 0) {
-		    logerror("country %d at level %d with country %d.\n",
-			     n, cont, cn);
-		    setcont(n, cn, cont - 1);
-		}
+	    for (cn = 1; cn < MAXNOC; cn++) {
+		if ((cnp = getnatp(cn)) != NULL)
+		    agecontact(cnp);
 	    }
 	}
     }

src/lib/update/main.c
@@ -179,13 +179,6 @@ update_main(void *unused)
 	    }
 	}
     }
-    /* Age contact */
-    if (opt_LOSE_CONTACT) {
-	for (cn = 1; cn < MAXNOC; cn++) {
-	    if ((cnp = getnatp(cn)) != NULL)
-		agecontact(cnp);
-	}
-    }
     /* Only update mobility for non-MOB_ACCESS here, since it doesn't
        get done for MOB_ACCESS anyway during the update */
     if (!opt_MOB_ACCESS) {

commit 8f3cfb8ffc57c48267a2bc90de77195b50922d77

update: Fix excessively fast contact aging
When option LOSE_CONTACT is enabled, contact is lost after a few
updates.  The remaining number of updates is to be decremented by one
each update.  However, it's actually decremented every time an active
nation is updated.  This makes countries lose contact much too fast,
typically every update.  Broken in commit ac25058, v4.3.0.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

src/lib/update/nat.c
@@ -134,8 +134,6 @@ prod_nat(int etu)
     double newvalue;
     struct budg_item *bm;
     natid n;
-    int cn;
-    struct natstr *cnp;
 
     for (n = 0; NULL != (np = getnatp(n)); n++) {
 	grant_btus(np, game_reset_tick(&np->nat_access));
@@ -182,12 +180,14 @@ prod_nat(int etu)
 	wu(0, n, "total pop was %d, yielding %4.2f hap, %4.2f edu\n",
 	   pop - 1, hap, edu);
     }
+
     if (ally_factor > 0.0)
 	share_incr(res, tech);
     else {
 	memset(res, 0, sizeof(res));
 	memset(tech, 0, sizeof(tech));
     }
+
     for (n = 0; NULL != (np = getnatp(n)); n++) {
 	if (np->nat_stat < STAT_ACTIVE)
 	    continue;
@@ -221,13 +221,11 @@ prod_nat(int etu)
 	np->nat_money = roundavg(nat_budget[n].money);
 	wu(0, n, "money delta was $%d for this update\n",
 	   np->nat_money - nat_budget[n].start_money);
+    }
 
-	if (opt_LOSE_CONTACT) {
-	    for (cn = 1; cn < MAXNOC; cn++) {
-		if ((cnp = getnatp(cn)) != NULL)
-		    agecontact(cnp);
-	    }
-	}
+    if (opt_LOSE_CONTACT) {
+	for (n = 0; NULL != (np = getnatp(n)); n++)
+	    agecontact(np);
     }
 }
 